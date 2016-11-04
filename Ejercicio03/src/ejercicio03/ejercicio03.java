7dc4004da9531542246277c3c325f9ea58b5c335
christianmantuano <christian.mantuano.raza@gmail.com>
2016-11-04 19:26:49 +0100

    ejercicio 3

---
+++ Ejercicio03/src/ejercicio03/ejercicio03.java
@@ -0,0 +1,15 @@
+
+package ejercicio03;
+import java.util.Scanner;
+public class ejercicio03 {
+
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+		int n,
+		indice=1;
+		multiplicador=0;
+		System.out.println("numero");
+		n= teclado.nextInt();
+	}
+
+}
