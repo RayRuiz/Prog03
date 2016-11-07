ceb27942326aa92da5dce8d1c1fba9131e34317f
RayRuiz <ixray110312@gmail.com>
2016-11-07 19:25:57 +0100

    dsf

---
+++ Ejercicio05/src/Ejercicio05.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner (System.in);
+		int numero,suma;
+		suma=0;
+		System.out.println("Ingrese un numero");
+		numero=teclado.nextInt();
+		if (numero!=0){
+			
+			while (numero!=0){
+				suma=suma+numero;
+				System.out.println(x);
+			}
+		}
+
+	}
+
+}
